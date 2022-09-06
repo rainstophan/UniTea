97dafa445761fc98297d1e2b3d92db8e1d733ca9
Yuting Han <49219934+rainstophan@users.noreply.github.com>
2022-09-06 20:49:04 +0200

    Create Animation.cs

---
+++ Animation.cs
@@ -0,0 +1,8 @@
+1. Import fbx file
+2. Rig: Apply Humanoid & Confogure avatar
+3. Animation: Set actual end frame & Set loop and root transform 
+
+1. Creat animator
+2. Blend tree (1D || 2D)
+
+1. Scripts Charactor controller
