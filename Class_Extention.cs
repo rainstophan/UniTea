46d39f6a07fb6ad3dc1a77c20179d59ad696ed6f
Yuting Han <49219934+rainstophan@users.noreply.github.com>
2022-12-31 01:56:21 +0100

    Create Class_Extention.cs

---
+++ Class_Extention.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+// 1
+namespace CustomExtensions 
+{   
+// 2   
+  public static class StringExtensions
+  {       
+      // 3      
+      public static void FancyDebug(this string str) 
+      {           
+          // 4          
+          Debug.LogFormat("This string contains {0} characters.",str.Length);
+      } 
+   }
+}
+
+// **************************
+
+
+// 1
+using CustomExtensions;
+
+
+public void Initialize(){       
+    _state = "Manager initialized..";        
+    // 2       
+    _state.FancyDebug();
+    Debug.Log(_state);    
+}
